c60f5be977ed440ed45831f3b08e2a17fdf08747
merestrickland <mere.strickland@gmail.com>
2020-01-17 17:20:42 -0500

    color change

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <style>
+        @import url('https://fonts.googleapis.com/css?family=Stoke&display=swap');
+      </style>
+      <header className="App-header">
+        
+      </header>
+      <div className='intro'>
+        <div className='hello'><h1>HELLO.</h1></div>
+        <div className='name'><h1>My name is Meredith.</h1></div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
