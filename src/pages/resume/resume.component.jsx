3fd15ae3802563b63afd6d97ea9f6b99301224bc
merestrickland <mere.strickland@gmail.com>
2020-03-05 15:18:37 -0500

    latida styling experiments

--- src/pages/resume/resume.component.jsx
+++ src/pages/resume/resume.component.jsx
@@ -3,6 +3,8 @@
 // pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 import React from 'react'
+import NavList from '../../components/navlist/navlist.component'
+import './resume.styles.css'
 
 export default class Resume extends React.Component {
 
@@ -23,9 +25,14 @@ export default class Resume extends React.Component {
 
   render() {
     return (
+      <>
+      {/* <div className='navlist-container'>
+        <NavList className='navlist'/>
+      </div> */}
       <div ref={this.viewerRef} id='viewer' style={{ width: '100%', height: '1000px' }}>
 
       </div>
+      </>
     )
   }
 }
