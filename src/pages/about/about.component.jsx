4a38b154a7a6f6b0ea01c23b4ce238136514460d
merestrickland <mere.strickland@gmail.com>
2020-04-01 14:21:09 -0400

    increased size of email me sentence

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -17,7 +17,8 @@ export default function About() {
             <img src='https://meredith-strickland-portfolio.s3.amazonaws.com/Portfolio/meme.png'/>
           </LazyLoad>
           <p className='title'>My name is Meredith.</p>
-          <p>I am a full stack developer. My skills include JavaScript, React, Ruby on Rails, Node, Express.js, MongoDB, HTML, CSS, Git, and I am currently learning Python. I have a background in Architecture, which I left because I want to design for everyone, not just for the people who can afford it. I am from New Orleans, I went to college in Vermont, and I live in Brooklyn. Email me at mere.strickland@gmail.com </p>
+          <p>I am a full stack developer. My skills include JavaScript, React, Ruby on Rails, Node, Express.js, MongoDB, HTML, CSS, Git, and I am currently learning Python. I have a background in Architecture, which I left because I want to design for everyone, not just for the people who can afford it. I am from New Orleans, I went to college in Vermont, and I live in Brooklyn.  </p>
+          <p className='email'>Email me at mere.strickland@gmail.com</p>
           {/* My favorite food is crawfish, my second favorite food is popsicles. My favorite book is "Far From the Tree" by Andrew Solomon.  */}
         </div>
       </div>
