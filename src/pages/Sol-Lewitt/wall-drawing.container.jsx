268a8d8e2c39264fa6bc835db70aaec59e5c3bdc
merestrickland <mere.strickland@gmail.com>
2020-07-15 14:00:00 +0100

    css trouble

--- src/pages/Sol-Lewitt/wall-drawing.container.jsx
+++ src/pages/Sol-Lewitt/wall-drawing.container.jsx
@@ -5,17 +5,23 @@ import './wall-drawing.styles.css'
 import '../../../src/index.css'
 import P5Wrapper from 'react-p5-wrapper';
 import LeftSide from '../../components/left-side/left-side.component'
+import NavBar from '../../components/navbar/navbar.component'
 
 const WallDrawing = props => {
 
 
     return (
+        
         <div className='container'>
+            <div className='nav'>
+                {/* <NavBar /> */}
+            </div>
 
             <div className='left'>
+            
                 <LeftSide />
             </div>
-            
+
             <div className='right'>
             <marquee><a href='https://medium.com/@mere.strickland/create-your-own-sol-lewitt-with-p5-js-165cdeda2d88'>Read my blog post</a> to find out how I coded a Sol Lewitt drawing with P5.js</marquee>
             <div className='image'>
