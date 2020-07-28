8d9f350b888ddac81bf12e2c8da3d778081ca05f
merestrickland <mere.strickland@gmail.com>
2020-07-28 14:55:00 -0500

    updated resume

--- src/components/projectList.js
+++ src/components/projectList.js
@@ -1,6 +1,6 @@
 
 const projectList = [
-  // {"_id":1,"name":"Monster Money","description":"An app to teach teenagers financial literacy through the care of a monster","github":"https://github.com/Paper-Trail-Project","media":"https://meredith-strickland-portfolio.s3.amazonaws.com/Portfolio/Screen+Shot+2020-07-10+at+1.45.04+PM.png", "component": "walldrawing"},
+  {"_id":1,"name":"Monster Money","description":"An app to teach teenagers financial literacy through the care of a monster","github":"https://github.com/Paper-Trail-Project","media":"https://meredith-strickland-portfolio.s3.amazonaws.com/Portfolio/Screen+Shot+2020-07-10+at+1.45.04+PM.png", "component": "walldrawing"},
 
   {"_id":2,"name":"Blobbers","description":"An interactive JavaScript SPA where the user gains points by grabbing pieces of the sun, or loses points by running into the falling rain","demoVid":"https://www.youtube.com/watch?v=CHfE7pHK0gU","toolsUsed":"JavaScript, P5.js","github":"https://github.com/jralorro93/blobbers-frontend","media":"https://meredith-strickland-portfolio.s3.amazonaws.com/Portfolio/Blobbers.mp4","component": "mediademo"},
 
@@ -10,7 +10,7 @@ const projectList = [
 
   {"_id":5,"name":"Wall Drawing","description":"Sol Lewitt was a conceptual artist, and is well-known for his wall drawings which can be created by anyone following the instructions he has outlined. This leaves a lot of room for interpretation by the person creating the drawing. Here, I used P5.js to execute his Wall Drawing #17, in which the only instructions are \"Four-part drawing with a different line direction in each part\".","toolsUsed":"JavaScript, HTML, P5.js","github":"https://github.com/merestrickland/P5-Hello/blob/master/src/index.js","image":"https://meredith-strickland-portfolio.s3.amazonaws.com/Portfolio/sol.png", "component": "walldrawing"},
 
-  {"_id":6,"name":"Tr(aid)","description":"A web app that empowers members of your community to offer support for those in need, or to ask for resources from others. Winner of the Essteem Equalithon.","demoVid":"https://tr-aid.web.app/","toolsUsed":"JavaScript, React, AWS Firebase, Heroku","github":"https://github.com/equalithon-team-three","image":"https://meredith-strickland-portfolio.s3.amazonaws.com/Portfolio/traid.png"},
+  {"_id":6,"name":"Tr(aid)","description":"A web app that empowers members of your community to offer support for those in need, or to ask for resources from others. Winner of the Essteem Equalithon.","demoVid":"https://tr-aid.web.app/","toolsUsed":"JavaScript, React, AWS Firebase, Heroku","github":"https://github.com/equalithon-team-three","media":"https://www.youtube.com/watch?v=_QVQUhjbVqs&feature=emb_logo", "component": "mediademo"},
 
   {"_id":7,"name":"Gro Cart","description":"a CLI application using Ruby to create shopping lists","demoVid":"https://www.youtube.com/watch?v=SylcQAxbskc","media":"https://meredith-strickland-portfolio.s3.amazonaws.com/Portfolio/grocart-demo-480.mov", "component": "mediademo"},
 ]
