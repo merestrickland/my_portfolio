1921ddec0aee0adca40e68a7805724bd97bbfbbe
merestrickland <mere.strickland@gmail.com>
2020-07-30 16:49:30 -0500

    basic styling for project tiles

--- src/components/projectList.js
+++ src/components/projectList.js
@@ -1,10 +1,10 @@
 
 const projectList = [
-  {"_id":1,"name":"Monster Money","description":"An app to teach teenagers financial literacy through the care of a monster","github":"https://github.com/Paper-Trail-Project","media":"https://meredith-strickland-portfolio.s3.amazonaws.com/Portfolio/Screen+Shot+2020-07-10+at+1.45.04+PM.png", "component": "walldrawing"},
+  {"_id":1,"name":"Monster Money","description":"An app to teach teenagers financial literacy through the care of a monster","github":"https://github.com/Paper-Trail-Project","media":"https://meredith-strickland-portfolio.s3.amazonaws.com/Portfolio/Screen+Shot+2020-07-10+at+1.45.04+PM.png", "component": "walldrawing", "image_url": "none"},
 
   {"_id":2,"name":"Blobbers","description":"An interactive JavaScript SPA where the user gains points by grabbing pieces of the sun, or loses points by running into the falling rain","demoVid":"https://www.youtube.com/watch?v=CHfE7pHK0gU","toolsUsed":"JavaScript, P5.js","github":"https://github.com/jralorro93/blobbers-frontend","media":"https://meredith-strickland-portfolio.s3.amazonaws.com/Portfolio/Blobbers.mp4","component": "mediademo"},
 
-  {"_id":3,"name":"Trip List","description":"A website where users can add recommendations of things to do on upcoming or previous trips","demoVid":"https://www.youtube.com/watch?v=D8qmIOkQbvY","toolsUsed":"JavaScript, React, Redux, Ruby on Rails","github":"https://github.com/merestrickland/TripList-frontend","media":"https://meredith-strickland-portfolio.s3.amazonaws.com/Portfolio/TripList+Demo.mp4", "component": "mediademo"},
+  {"_id":3,"name":"Trip List","description":"A website where users can add recommendations of things to do on upcoming or previous trips","demoVid":"https://www.youtube.com/watch?v=D8qmIOkQbvY","toolsUsed":"JavaScript, React, Redux, Ruby on Rails","github":"https://github.com/merestrickland/TripList-frontend","media":"https://meredith-strickland-portfolio.s3.amazonaws.com/Portfolio/TripList+Demo.mp4", "component": "mediademo", "image_url": "https://meredith-strickland-portfolio.s3.amazonaws.com/Portfolio/triplist.png"},
 
   {"_id":4,"name":"Rita","description":"Prototype for an app that helps caregivers communicate with each other and keep track of appointments and medications. Winner of the \"People's Choice Award\" at LYLAS Labs: Women at Work Hackathon","demoVid":"https://www.youtube.com/watch?v=SylcQAxbskc","media":"https://meredith-strickland-portfolio.s3.amazonaws.com/Portfolio/Rita+Demo1.mp4", "component": "mediademo"},
 
